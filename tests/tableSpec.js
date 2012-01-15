997cba3f07221cfea8fc28f10ca30d94fde07e5f
Russ <russplaysguitar@yahoo.com>
2012-01-15 15:18:23 -0800

    Added tableSpec.coffee and tableSpec.js

---
+++ tests/tableSpec.js
@@ -0,0 +1,60 @@
+(function() {
+
+  describe("Table", function() {
+    beforeEach(function() {
+      var x;
+      x = [
+        {
+          one: 1,
+          two: "2",
+          three: 3,
+          four: "4"
+        }, {
+          one: 10,
+          two: "20",
+          three: 30,
+          four: "40"
+        }
+      ];
+      return this.table = window.to_table(x);
+    });
+    it("Should be an object", function() {
+      return expect(typeof this.table).toBe('object');
+    });
+    it("Should have columns", function() {
+      return expect(this.table.cols).toBeDefined();
+    });
+    it("Should have rows", function() {
+      return expect(this.table.rows).toBeDefined();
+    });
+    it("Should have correctly ordered columns", function() {
+      var cols;
+      cols = this.table.cols;
+      expect(cols[0]).toBe("one");
+      expect(cols[1]).toBe("two");
+      expect(cols[2]).toBe("three");
+      return expect(cols[3]).toBe("four");
+    });
+    it("Should have correctly ordered rows", function() {
+      var row;
+      row = this.table.rows[0];
+      expect(row[0]).toBe(1);
+      expect(row[1]).toBe("2");
+      expect(row[2]).toBe(3);
+      return expect(row[3]).toBe("4");
+    });
+    it("Should have multiple rows", function() {
+      expect(this.table.rows[0]).toBeDefined();
+      return expect(this.table.rows[1]).toBeDefined();
+    });
+    return it("Should have correct data", function() {
+      var expected;
+      expected = {
+        cols: ["one", "two", "three", "four"],
+        rows: [[1, "2", 3, "4"], [10, "20", 30, "40"]]
+      };
+      return expect(this.table).toEqual(expected);
+    });
+  });
+
+}).call(this);
